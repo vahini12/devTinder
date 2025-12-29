7b869aedfce292fc1f233602f68b1966dc438b59
APAC\VMH8KOR <manku.vahini@in.bosch.com>
2025-12-29 18:46:14 +0530

    request handles in express

--- src/app.js
+++ src/app.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const app = express()
 
-app.use('/test',(req,res)=>{
-    res.send("Hello from server")
+
+
+
+app.get('/user',(req,res)=>{
+    res.send({firstname :"Vahini", lastname : "Manku"})
 })
+
+app.post('/user',(req,res)=>{
+    res.send("USer is saved successfully")
+})
+app.delete('/user',(req,res)=>{
+    res.send("User is deleted successfully")
+})
+
 app.listen(3000, ()=>{
     console.log("server is running successfully on port 3000")
 })
