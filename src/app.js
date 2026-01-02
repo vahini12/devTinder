44b9ecf02bc8c263fa3958bafd1810e853b1acb4
APAC\VMH8KOR <manku.vahini@in.bosch.com>
2026-01-02 17:35:32 +0530

    error handling in middleware

--- src/app.js
+++ src/app.js
@@ -2,36 +2,16 @@ const express = require("express");
 const { adminAuth ,userAuth} = require("./middlewares/auth");
 const app = express()
 
+app.get('/getUSerData',(req,res)=>{
 
+    throw new Error("Data is not present")
+})
 
-
-app.use("/admin",adminAuth)
-
-
-app.get("/admin/getAllData",(req,res)=>{
-   
-    res.send("Sent Data successfully")
-    
- })
-
- 
-app.get("/admin/deleteData",(req,res)=>{
-
-    res.send("data deleted successfully")
-    
- })
-
- app.get("/user/login", (req,res)=>{
-
-    res.send("user logged  successfully")
-    
- })
- 
- app.get("/user",userAuth, (req,res)=>{
-
-    res.send("user data send  successfully")
-    
- })
+app.use('/', (err,req,res,next)=> {
+    if(err){
+        res.status(500).send("Some error is thrown from middleware")
+    }
+})
 
 
 
